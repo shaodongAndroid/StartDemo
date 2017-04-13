b2d19af934de7c9b60ed499a54ed2795cd648f62
dsd <xiayuledsd@163.com>
2017-04-13 11:31:07 +0800

    添加 Hello 文件

--- app/src/main/java/com/start/demo/MainActivity.java
+++ app/src/main/java/com/start/demo/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             default:
+                Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
---
+++ app/src/main/java/com/start/demo/adapter/Hello.java
@@ -0,0 +1,11 @@
+package com.start.demo.adapter;
+
+/**
+ * Created by dsd on 2017/4/13.
+ */
+
+public class Hello {
+    public Hello(){
+
+    }
+}
