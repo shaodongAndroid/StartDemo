e169cc8d10059810b5ae7f0234a4b4001225ad0e
dsd <xiayuledsd@163.com>
2017-04-13 11:29:17 +0800

    调试

--- app/src/main/java/com/start/demo/MainActivity.java
+++ app/src/main/java/com/start/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.start.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -10,12 +11,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+
+    private void initView() {
+        findViewById(R.id.btn_onclick).setOnClickListener(this);
+    }
+
 
     @Override public void onClick(View v) {
         switch (v.getId()){
+            case R.id.btn_onclick:
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+                break;
+
             default:
                 break;
         }
+    }
+
+    public void test(){
+
+    }
+
+    public void test2(){
+
+    }
+    public void test3(){
+
+    }
+    public void test4(){
+
+    }
+    public void test5(){
+
     }
 }
