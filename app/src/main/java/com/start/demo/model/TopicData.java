1442a171364d9003d800488e1ce5f7424dcc6917
dsd <xiayuledsd@163.com>
2017-04-13 12:08:40 +0800

    修改TopicData

--- app/src/main/java/com/start/demo/model/TopicData.java
+++ app/src/main/java/com/start/demo/model/TopicData.java
@@ -5,4 +5,34 @@ package com.start.demo.model;
  */
 
 public class TopicData {
+
+    private int topicId ;
+
+    private String topicName ;
+
+    private String answer ;
+
+    public int getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(int topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
